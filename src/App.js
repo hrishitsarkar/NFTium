426ecf30e0c5977166a80e02087831c3bda5f390
nothrishit <hrishitjohn@gmail.com>
2023-08-31 14:15:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { Component } from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+
+export default function App() {
+  // Write this code in Navbar.js file
+
+
+  // Write this code in Hero.js file
+  
+
+  // Write this code in About.js file
+  
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
